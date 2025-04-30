b4b88f360bace47423452d695acb96c71a0b6469
EricMangrane <eric.mangrane@institutroquetes.cat>
2025-04-30 09:56:00 +0200

    window onload +

--- index.js
+++ index.js
@@ -95,43 +95,6 @@ function tanca_sessio() {
     }
 }
 
-window.onload = () => { 
-    let vegueries = [[41.39, 2.17, "Àmbit metropolità (Barcelona)"],    // llista on cada element és una llista amb els valors de latitud, longitud i nom de vegueria com a elements
-                 [42.17, 0.89, "Alt Pirineu i Aran (Tremp)"],
-                 [41.12, 1.24, "Camp de Tarragona (Tarragona)"],
-                 [41.73, 1.83 ,"Comarques centrals (Manresa)"],
-                 [41.98, 2.82, "Comarques gironines (Girona)"],
-                 [41.62, 0.62, "Ponent (Lleida)"],
-                 [40.81, 0.52, "Terres de l'Ebre (Tortosa)"],
-                 [41.35, 1.70, "Penedès (Vilafranca del Penedès"]];
-    let base_de_dades = storage.getItem("base_de_dades");   
-    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
-}).addTo(mapa);    // s'afegeix la capa al mapa
-    if(base_de_dades == null) {
-        indexedDB.open("Dades").onupgradeneeded = event => {   
-            event.target.result.createObjectStore("Fotos", {keyPath: "ID", autoIncrement:true}).createIndex("Usuari_index", "Usuari");
-        }    // les fotos es desen a la taula "Fotos"
-        storage.setItem("base_de_dades","ok");
-    }
-    document.getElementById("obturador").addEventListener("change", function() {    // procediment que s'executa quan s'obté el fitxer de la foto realitzada (esdeveniment "change")
-        if(this.files[0] != undefined) {    // instruccions que s'executen només si s'obté algun fitxer (només es processa el primer que es rebi)
-            let canvas = document.getElementById("canvas");    // contenidor on es desa temporalment la imatge
-            let context = canvas.getContext("2d");
-            let imatge = new Image;
-            imatge.src = URL.createObjectURL(this.files[0]);    // es crea la imatge a partir del fitxer
-            imatge.onload = () => {    // procediment que s'executa un cop la imatge s'ha carregat en el contenidor
-                canvas.width = imatge.width;
-                canvas.height = imatge.height;                
-                context.drawImage(imatge,0,0,imatge.width,imatge.height);    // es "dibuixa" la imatge en el canvas
-                document.getElementById("foto").src = canvas.toDataURL("image/jpeg");    // la imatge es mostra en format jpg
-                document.getElementById("icona_camera").style.display = "none";    // s'oculta la icona que hi havia abans de fer la foto
-                document.getElementById("desa").style.display = "unset";    // es mostra el botó per desar la foto
-            }
-        }
-    });
-}
 
 function desa_foto() {
     let nou_registre = {    // contingut del nou registre de la base de dades
@@ -224,6 +187,44 @@ function esborra_foto(id) {
             };
         };
     }
+}
+
+window.onload = () => { 
+    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
+}).addTo(mapa);    // s'afegeix la capa al mapa
+    let vegueries = [[41.39, 2.17, "Àmbit metropolità (Barcelona)"],    // llista on cada element és una llista amb els valors de latitud, longitud i nom de vegueria com a elements
+                 [42.17, 0.89, "Alt Pirineu i Aran (Tremp)"],
+                 [41.12, 1.24, "Camp de Tarragona (Tarragona)"],
+                 [41.73, 1.83 ,"Comarques centrals (Manresa)"],
+                 [41.98, 2.82, "Comarques gironines (Girona)"],
+                 [41.62, 0.62, "Ponent (Lleida)"],
+                 [40.81, 0.52, "Terres de l'Ebre (Tortosa)"],
+                 [41.35, 1.70, "Penedès (Vilafranca del Penedès"]];
+    let base_de_dades = storage.getItem("base_de_dades");   
+    if(base_de_dades == null) {
+        indexedDB.open("Dades").onupgradeneeded = event => {   
+            event.target.result.createObjectStore("Fotos", {keyPath: "ID", autoIncrement:true}).createIndex("Usuari_index", "Usuari");
+        }    // les fotos es desen a la taula "Fotos"
+        storage.setItem("base_de_dades","ok");
+    }
+    document.getElementById("obturador").addEventListener("change", function() {    // procediment que s'executa quan s'obté el fitxer de la foto realitzada (esdeveniment "change")
+        if(this.files[0] != undefined) {    // instruccions que s'executen només si s'obté algun fitxer (només es processa el primer que es rebi)
+            let canvas = document.getElementById("canvas");    // contenidor on es desa temporalment la imatge
+            let context = canvas.getContext("2d");
+            let imatge = new Image;
+            imatge.src = URL.createObjectURL(this.files[0]);    // es crea la imatge a partir del fitxer
+            imatge.onload = () => {    // procediment que s'executa un cop la imatge s'ha carregat en el contenidor
+                canvas.width = imatge.width;
+                canvas.height = imatge.height;                
+                context.drawImage(imatge,0,0,imatge.width,imatge.height);    // es "dibuixa" la imatge en el canvas
+                document.getElementById("foto").src = canvas.toDataURL("image/jpeg");    // la imatge es mostra en format jpg
+                document.getElementById("icona_camera").style.display = "none";    // s'oculta la icona que hi havia abans de fer la foto
+                document.getElementById("desa").style.display = "unset";    // es mostra el botó per desar la foto
+            }
+        }
+    });
 }
 
 for (i in vegueries) {    // per cada element de la llista
